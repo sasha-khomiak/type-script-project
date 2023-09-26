444253b8f0c67123cffe9070fb6dbd7f3ce82fca
Oleksandr Khomiak <khomiak@gmail.com>
2023-09-26 18:37:59 +0300

    first init

---
+++ app.js
@@ -0,0 +1,5 @@
+function sayHello(name) {
+    console.log("Hello, ".concat(name, "!"));
+}
+var personName = "Alex";
+sayHello(personName);
---
+++ app.ts
@@ -0,0 +1,6 @@
+function sayHello(name: string): void {
+  console.log(`Hello, ${name}!`);
+}
+
+const personName: string = "Alex";
+sayHello(personName);
