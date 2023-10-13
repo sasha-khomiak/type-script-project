7232988daf1a20b890b024dec6a4862486755fcc
Oleksandr Khomiak <khomiak@gmail.com>
2023-10-13 20:54:26 +0300

    etst incapulation js

--- dist/app.js
+++ dist/app.js
@@ -1,20 +1,13 @@
-class Car {
-    // Конструктор класу
-    constructor(brand, model, year) {
-        this.brand = brand;
-        this.model = model;
-        this.year = year;
-    }
-    // Метод класу (функція)
-    displayInfo() {
-        return `Це ${this.year} ${this.brand} ${this.model}.`;
-    }
+function protectedMethod() {
+  return "Something";
 }
-// Створення об'єкта класу Car
-let myCar1 = new Car("Toyota", "Corolla", 2021);
-let myCar2 = new Car("Ford", "EDGE", 2019);
-// Виклик методу об'єкта
-console.log(myCar1.displayInfo()); // Виведе "Це 2021 Toyota Corolla."
-console.log(myCar2.displayInfo()); // Виведе "Це 2019 Ford EDGE."
-console.log("myCar1", myCar1);
-//# sourceMappingURL=app.js.map
+
+class myClass {
+  myPublicMethod() {
+    return protectedMethod();
+  }
+}
+
+let myObj = new myClass();
+
+console.log(myObj.myPublicMethod());
