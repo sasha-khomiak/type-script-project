1fdfeaecbb1ef0781da7236a286ef30fb1861437
Oleksandr Khomiak <khomiak@gmail.com>
2023-10-17 18:01:47 +0300

    interfaces

--- dist/app.js
+++ dist/app.js
@@ -1,18 +1,7 @@
-class Shape {
-    // Звичайний метод з реалізацією
-    printArea() {
-        console.log("Площа фігури: " + this.calculateArea());
-    }
-}
-class Circle extends Shape {
-    constructor(radius) {
-        super();
-        this.radius = radius;
-    }
-    // Реалізація абстрактного методу
-    calculateArea() {
-        return Math.PI * this.radius * this.radius;
-    }
-}
-let circle = new Circle(5);
-circle.printArea(); // Виведе "Площа фігури: 78.54"
+// // інтерфейс 1
+// interface Person {
+//   name: string;
+//   age: number;
+const person = {
+    name: "Alex",
+};
--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,64 @@
-abstract class Shape {
-  abstract calculateArea(): number; // Абстрактний метод без реалізації
+// // інтерфейс 1
+// interface Person {
+//   name: string;
+//   age: number;
 
-  // Звичайний метод з реалізацією
-  printArea(): void {
-    console.log("Площа фігури: " + this.calculateArea());
-  }
+//   information(): void;
+// }
+
+// // інтерфейс 2
+// interface Feedback {
+//   getName(): string;
+// }
+
+// // Клас
+// class TeamWorker implements Person, Feedback {
+//   constructor(
+//     public name: string,
+//     public age: number,
+//     public surname?: string
+//   ) {}
+
+//   information(): void {
+//     console.log("This is " + this.name + "! He is " + this.age + " years old.");
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+
+//   setSurname(value: string) {
+//     this.surname = value;
+//   }
+// }
+
+// // обʼєкт класу
+// let teamLead = new TeamWorker("Alex", 25);
+
+// teamLead.information(); // This is Alex! He is 25 years old.
+// console.log(teamLead.getName()); // "Alex"
+
+// // let person: Person = {
+// //   name: "John",
+// //   age: 30,
+
+// //   information() {
+// //     console.log("This is " + this.name + "! He is " + this.age + " years old.");
+// //   },
+// // };
+
+// // person.information();
+
+interface Person {
+  readonly name: string;
 }
 
-class Circle extends Shape {
-  constructor(private radius: number) {
-    super();
-  }
+interface TeamWorker extends Person {
+  readonly surName: string;
 
-  // Реалізація абстрактного методу
-  calculateArea(): number {
-    return Math.PI * this.radius * this.radius;
-  }
+  showWorker();
 }
 
-let circle = new Circle(5);
-circle.printArea(); // Виведе "Площа фігури: 78.54"
+const person: Person = {
+  name: "Alex",
+};
