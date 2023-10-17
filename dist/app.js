629b84283eedaef151bdc2d860bfe78e6cff0a68
Oleksandr Khomiak <khomiak@gmail.com>
2023-10-17 12:46:33 +0300

    nasliduvannia

--- dist/app.js
+++ dist/app.js
@@ -1,12 +1,33 @@
-class Circle {
-    constructor(radius) {
-        this.radius = radius;
+// Базовий клас (суперклас)
+class Animal {
+    constructor(name) {
+        this.name = name;
     }
-    calculateArea() {
-        return Math.PI * this.radius * this.radius;
+    // Метод базового класу
+    makeSound() {
+        console.log("Some sound");
     }
 }
-let circle = new Circle(5);
-console.log(circle.radius); // Виведе 5
-// Спроба змінити значення radius призведе до помилки
-// circle.radius = 10; // Помилка: властивість "radius" тільки для читання
+// Підклас, який успадковує клас Animal
+class Dog extends Animal {
+    constructor(name, breed) {
+        // Виклик конструктора базового класу за допомогою super()
+        super(name);
+        this.breed = breed;
+    }
+    // Перевизначений метод базового класу
+    makeSound() {
+        console.log("Bark!");
+    }
+    // Новий метод підкласу
+    wagTail() {
+        console.log("Tail is wagging");
+    }
+}
+// Створення об'єкта підкласу
+let myDog = new Dog("Buddy", "Golden Retriever");
+// Виклик методів базового та підкласів
+console.log(myDog.name); // Виведе "Buddy"
+console.log(myDog.breed); // Виведе "Golden Retriever"
+myDog.makeSound(); // Виведе "Bark!"
+myDog.wagTail(); // Виведе "Tail is wagging"
--- src/app.ts
+++ src/app.ts
@@ -1,17 +1,44 @@
-class Circle {
-  readonly radius: number;
+// Базовий клас (суперклас)
+class Animal {
+  name: string;
 
-  constructor(radius: number) {
-    this.radius = radius;
+  constructor(name: string) {
+    this.name = name;
   }
 
-  calculateArea(): number {
-    return Math.PI * this.radius * this.radius;
+  // Метод базового класу
+  makeSound(): void {
+    console.log("Some sound");
   }
 }
 
-let circle = new Circle(5);
-console.log(circle.radius); // Виведе 5
+// Підклас, який успадковує клас Animal
+class Dog extends Animal {
+  // Додаткова властивість підкласу
+  breed: string;
 
-// Спроба змінити значення radius призведе до помилки
-// circle.radius = 10; // Помилка: властивість "radius" тільки для читання
+  constructor(name: string, breed: string) {
+    // Виклик конструктора базового класу за допомогою super()
+    super(name);
+    this.breed = breed;
+  }
+
+  // Перевизначений метод базового класу
+  makeSound(): void {
+    console.log("Bark!");
+  }
+
+  // Новий метод підкласу
+  wagTail(): void {
+    console.log("Tail is wagging");
+  }
+}
+
+// Створення об'єкта підкласу
+let myDog = new Dog("Buddy", "Golden Retriever");
+
+// Виклик методів базового та підкласів
+console.log(myDog.name); // Виведе "Buddy"
+console.log(myDog.breed); // Виведе "Golden Retriever"
+myDog.makeSound(); // Виведе "Bark!"
+myDog.wagTail(); // Виведе "Tail is wagging"
